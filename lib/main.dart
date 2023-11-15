b91f6195bab0aee096153baebd39c034f82dd01e
unknown <george.535220173@stu.untar.ac.id>
2023-11-15 12:27:17 +0700

    udah mau akhir

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyHomePage extends StatelessWidget {
           children: [
             Text('Godrej'),
             Padding(
-              padding: const EdgeInsets.only(left: 160),
+              padding: const EdgeInsets.only(left: 165),
               child: Row(
                 children: [
                   Icon(Icons.search),
@@ -94,37 +94,85 @@ class MyHomePage extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text('Semua Produk',style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text(
+                    'Semua Produk',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ],
               ),
             ),
-              Padding(
+            Padding(
               padding: const EdgeInsets.all(16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text('Promosi', style: TextStyle(fontWeight: FontWeight.bold),),
-                  Text('Nama Produk', style: TextStyle(fontWeight: FontWeight.bold), 
+                  Text(
+                    'Promosi',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Nama Produk',
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                   Icon(Icons.arrow_upward),
-                  Text('Terlaris', style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text(
+                    'Terlaris',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ],
               ),
             ),
             // Detail produk
             Padding(
               padding: const EdgeInsets.all(16.0),
+              child: Wrap(
+                spacing: 16.0,
+                runSpacing: 16.0,
+                children: [
+                  buildProductCard('Susu Dancow', 'https://www.dancow.co.id/sites/default/files/2023-07/20230228_WL_NI_DANCOW%20FORTIGRO%20NEWSCALE%20IN%20780GR_A1_0003_1.png', 20000.0),
+                  buildProductCard('Beras Sania', 'https://pangan.pastisania.com/app/uploads/2022/02/Beras-sania-25kg.png', 25000.0),
+                  buildProductCard('Minyak Sania', 'https://assets.klikindomaret.com/products/10010804/10010804_1.jpg', 28000.0),
+                  buildProductCard('Stella Car Perfurme', 'https://stellacms.stella-airfreshener.com//upload-gambar-produk/produk/H25lBw_Stella%20Car%20Perfume%20-%20Energyc%20Purple.png', 41500.0),
+                  buildProductCard('Gajah Minyak Kayu Putih', 'https://images.k24klik.com/product/large/apotek_online_k24klik_20210504101159359225_GAJAH-MKP-6OML.jpg', 15300.0),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildProductCard(String name, String imageUrl, double price) {
+    return Container(
+      width: 150,
+      child: Card(
+        elevation: 10,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              imageUrl,
+              height: 104,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('Detail Produk 1'),
-                  // Informasi detail produk 1
-                  // ...
-                  SizedBox(height: 16),
-                  Text('Detail Produk 2'),
-                  // Informasi detail produk 2
-                  // ...
-                  // dan seterusnya
+                  Text(
+                    name,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text('\RP $price'),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Aksi ketika tombol keranjang ditekan
+                    },
+                    child: Text('+ Keranjang'),
+                  ),
                 ],
               ),
             ),
